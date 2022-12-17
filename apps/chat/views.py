ec4e8f08d6be26ba9f403ec16fa258ff0d79bf13
Kenechi Ifeanyi <kayprogrammer1@gmail.com>
2022-12-18 00:19:15 +0100

    feat: sending messages

--- apps/chat/views.py
+++ apps/chat/views.py
@@ -3,8 +3,10 @@ from flask_login import current_user
 from sqlalchemy import or_, case, literal_column
 from apps.accounts.decorators import login_required
 from apps.accounts.models import User
+from setup.extensions import db
 from . models import Message
 import json
+import pytz
 
 chat_router = Blueprint('chat_router', __name__, template_folder="templates")
 
@@ -28,11 +30,12 @@ def home():
     )
     inbox_list = messages.order_by(
         other, Message.created_at.desc()
-    ).distinct(other).order_by(Message.created_at.desc())
+    ).distinct(other)
+    sorted_inbox_list = sorted(inbox_list, key=lambda x: x.created_at, reverse=True)
     
     all_users = User.query.filter(User.is_email_verified==True, User.is_phone_verified==True, User.is_active==True, User.id != user.id).order_by('name')
 
-    return render_template('chat/index.html', user=current_user, all_users=all_users, inbox_list=inbox_list, messages=messages)
+    return render_template('chat/index.html', user=current_user, all_users=all_users, inbox_list=sorted_inbox_list, messages=messages)
 
 @chat_router.route('/show-direct-messages', methods=['POST'])
 def show_dms():
@@ -55,43 +58,44 @@ def show_dms():
     
     return jsonify(response)
 
-# class SendMessageView(LoginRequiredMixin, View):
-#     def post(self, request, *args, **kwargs):
-#         data = json.loads(request.body)
-#         user = request.user
-#         message = data.get('message')
-#         try:
-#             friend = User.objects.get(phone=data.get('phone'))
-#         except:
-#             return JsonResponse({'error': 'User not found'}) 
-#         if len(message) < 1:
-#             return JsonResponse({'error': "You didn't type anything"})
+@chat_router.route('/send-message', methods=['POST'])
+def send_message():
+    user = current_user
+    data = request.form
+    message = data.get('message')
+    friend = User.query.filter_by(phone=data.get('phone')).first()
+    if not friend:
+        return jsonify(error= 'User not found') 
+    if len(message) < 1:
+        return jsonify(error= "You didn't type anything")
 
-#         # Solve recent emojis
-#         recent_emojis = request.session.get('recent_emojis')
-#         em = []
-#         for i in message:
-#             if i in emojis:
-#                 if recent_emojis and i in recent_emojis:
-#                     recent_emojis.remove(i)
-#                 em.append(i)
+    # Solve recent emojis
+    recent_emojis = session.get('recent_emojis')
+    em = []
+    for i in message:
+        # if i in emojis:
+        if recent_emojis and i in recent_emojis:
+            recent_emojis.remove(i)
+        em.append(i)
             
 
-#         if len(em) > 0:
-#             em = list(set(em)) # remove duplicates
-#             if recent_emojis:
-#                 updated_emojis = em + recent_emojis
-#                 if len(updated_emojis) > 50:
-#                     n = len(updated_emojis) - 50
-#                     del updated_emojis[-n:]
-#                 request.session['recent_emojis'] = updated_emojis
-#             else:
-#                 updated_emojis = em
-#                 if len(updated_emojis) > 50:
-#                     n = len(updated_emojis) - 50
-#                     del updated_emojis[-n:]
-#                 request.session['recent_emojis'] = updated_emojis
+    if len(em) > 0:
+        em = list(set(em)) # remove duplicates
+        if recent_emojis:
+            updated_emojis = em + recent_emojis
+            if len(updated_emojis) > 50:
+                n = len(updated_emojis) - 50
+                del updated_emojis[-n:]
+            session['recent_emojis'] = updated_emojis
+        else:
+            updated_emojis = em
+            if len(updated_emojis) > 50:
+                n = len(updated_emojis) - 50
+                del updated_emojis[-n:]
+            session['recent_emojis'] = updated_emojis
 
-#         message_object = Message.objects.create(sender=user, receiver=friend, text=message)
-#         time = message_object.created_at.astimezone(pytz.timezone(request.user.tz.name))
-#         return JsonResponse({'success': True, 'message': message, 'time': time.strftime('%I:%M %p')})
+    message_object = Message(sender_id=user.id, receiver_id=friend.id, text=message)
+    db.session.add(message_object)
+    db.session.commit()
+    time = message_object.created_at.astimezone(pytz.timezone(user.tzname))
+    return jsonify(success=True, message= message, time= time.strftime('%I:%M %p'))
